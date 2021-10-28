a4f5ba4d4c720b74cd8a8cc3e2b65c22ef172638
chaitanya71998 <chaitanyaroy1@gmail.com>
2021-10-28 22:02:47 +0530

    added with copied changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
-import 'counter.dart';
-final counter = Counter();
-void main ()=>runApp(MyApp());
+import 'counter.dart'; // Import the Counter
 
+final counter = Counter(); // Instantiate the store
 
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "mobx",
+      title: 'MobX',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -30,24 +29,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:AppBar(
-          title: Text('mobx container'),
-        ),
-      body:  Center(
+      appBar: AppBar(
+        title: const Text('MobX Counter'),
+      ),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget> [
-            Text(
-              'You have pushed the button this many times:',
+          children: <Widget>[
+            const Text(
+              'You have blocked    the button this many times:',
             ),
-            Observer(builder: (_)=>{Text('${counter.value}',style: Theme.of(context).textTheme.headline4,,)})
+            // Wrapping in the Observer will automatically re-render on changes to counter.value
+            Observer(
+              builder: (_) => Text(
+                '${counter.value}',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: counter.increment,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
